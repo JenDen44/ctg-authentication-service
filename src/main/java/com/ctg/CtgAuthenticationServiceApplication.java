175a83be6b94f9db715587d1aefdc5e24cb62592
Evgenia Bullis <bulish2015@yandex.ru>
2025-09-15 18:53:06 +0300

    init

---
+++ src/main/java/com/ctg/CtgAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ctg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtgAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtgAuthenticationServiceApplication.class, args);
+	}
+
+}
